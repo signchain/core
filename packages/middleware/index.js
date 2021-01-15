7c2376f1bed2ceacab0a5152626774e37ab50589
Shubham-tiwari123 <tiwari.23.shubham@gmail.com>
2021-01-15 16:43:25 +0530

    adds threadDb creation for middleware

--- packages/middleware/index.js
+++ packages/middleware/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors')
-const socket = require('./services/socket-io');
+const socket = require('./services/threadDb/socket-io');
 const email = require('./services/email/sgEmail');
+const dbRoute = require('./services/threadDb/routes')
 
 const app = express()
 app.use(cors())
 app.use(express.json());
 dotenv.config()
 const PORT = process.env.PORT || 3001;
-
 
 const http = require('http').createServer(app);
 
@@ -21,6 +21,7 @@ const io = require('socket.io')(http,{
 
 email(app)
 socket(io)
+dbRoute(app)
 
 http.listen('3001', ()=>{
   console.log("Listening on port:", PORT)
