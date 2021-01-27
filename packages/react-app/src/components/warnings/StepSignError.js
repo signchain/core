761e82a64c864f0156fd5e037f4136c34001efae
koushith <koushith97@gmail.com>
2021-01-27 14:52:49 +0530

    adds all missing popup files

---
+++ packages/react-app/src/components/warnings/StepSignError.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Header, Icon, Modal } from "semantic-ui-react";
+
+function StepSignError({ signError, setSignError, setCurrent }) {
+  return (
+    <Modal closeIcon open={true} onClose={() => setSignError(false)} onOpen={() => setSignError(true)}>
+      <Header icon="warning sign" content="OOPS" />
+      <Modal.Content>
+        <p>There was an error while Signing. Do you want to Sign Again?</p>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button
+          color="red"
+          onClick={() => {
+            setSignError(false);
+            setCurrent(0);
+          }}
+        >
+          <Icon name="remove" /> No
+        </Button>
+
+        <Button color="green" onClick={() => setSignError(false)}>
+          <Icon name="checkmark" /> Yes
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
+}
+
+export default StepSignError;
