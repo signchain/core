c27ee1703b1563e5f203a921b660a56f22fd9815
koushith <koushith97@gmail.com>
2021-01-27 13:54:39 +0530

    fixes redirect issue

--- packages/react-app/src/components/Documents/DocumentDetails.js
+++ packages/react-app/src/components/Documents/DocumentDetails.js
@@ -2,16 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Table, Icon, Image, Button, Loader, Step, Label } from "semantic-ui-react";
 import SignWarning from "../warnings/SignWarning";
 import { WarningStatus, SignSuccess } from "./WarningNote";
-import {
-  DocumentContainer,
-  DocumentHeader,
-  HeaderContainer,
-  TitleHeading,
-  DocumentTable,
-  DetailsInfo,
-  DocsTitle,
-  DetailsContainer,
-} from "../styles/DocumentDetails.Style";
+import { DocumentContainer, DocumentHeader, DetailsInfo } from "../styles/DocumentDetails.Style";
 import {
   attachSignature,
   downloadFiles,
@@ -27,6 +18,7 @@ import Notarize from "../../images/icons/notarize.svg";
 import Attachments from "../../images/icons/attachments.svg";
 import LogoAvatar from "../../images/icons/logoavatar.svg";
 import jenny from "../../images/jenny.jpg";
+import SignSuccessPopUp from "../warnings/SignSuccessPopUp";
 
 const DocumentDetails = props => {
   const loggedUser = localStorage.getItem("USER");
@@ -38,8 +30,10 @@ const DocumentDetails = props => {
   const [document, setDocument] = useState(null);
   const [downloading, setDownloading] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [sign, setSign] = useState(false);
+  const [signWarning, setSignWarning] = useState(false);
   const [signLoader, setSignLoader] = useState(false);
+  const [signedStatus, setSignedStatus] = useState(false);
+  const [open, setOpen] = useState(true);
 
   useEffect(() => {
     async function load() {
@@ -60,7 +54,7 @@ const DocumentDetails = props => {
       }
     }
     load();
-  }, []);
+  }, [open, signedStatus]);
 
   const downloadFile = (name, key, location) => {
     console.log("Download btn clicked");
@@ -72,215 +66,242 @@ const DocumentDetails = props => {
   };
 
   const signDocument = async (docHash, docId) => {
+    setSignLoader(true);
     try {
       const result = await attachSignature(docId, props.userProvider.getSigner(), caller, docHash);
       console.log("RESULTTT", result);
-      if (result) setSign(false);
+      if (result) {
+        setSignedStatus(true);
+        setSignWarning(false);
+        setSignLoader(false);
+      }
     } catch (e) {
-      //alert("Didn't sign it!");
-      setSign(true);
+      // alert("Didn't sign it!");
+      setSignLoader(false);
+      setSignWarning(true);
     }
   };
 
   const notarizeDocument = async (docId, docHash) => {
-    const result = await notarizeDoc(
-      docId,
-      docHash,
-      props.tx,
-      props.writeContracts,
-      props.userProvider.getSigner(),
-      caller,
-    );
+    setSignLoader(true);
+    try {
+      const result = await notarizeDoc(
+        docId,
+        docHash,
+        props.tx,
+        props.writeContracts,
+        props.userProvider.getSigner(),
+        caller,
+      );
+      console.log("RESULTTT", result);
+      if (result) {
+        setSignedStatus(true);
+        setSignWarning(false);
+        setSignLoader(false);
+      }
+    } catch (e) {
+      // alert("Didn't sign it!");
+      setSignLoader(false);
+      setSignWarning(true);
+    }
   };
 
   return (
     <>
-      {sign ? (
-        <SignWarning sign={sign} setSign={setSign} />
+      {signedStatus ? (
+        <SignSuccessPopUp setOpen={setOpen} open={open} setSignedStatus={setSignedStatus} />
       ) : (
         <>
-          {" "}
-          <DocumentHeader>
-            {!loading ? (
-              <DetailsInfo>
-                <div class=" header-wrapper">
-                  <div class="Details-card-info header-container">
-                    <div class="title-left">
-                      <h3 className=" header-title">{document.title}</h3>
-                    </div>
-
-                    <div class="progress-container ">
-                      {/* conditional rendering */}
-                      {document.signStatus ? <Label color="green">Signed</Label> : <Label>Pending</Label>}
-                    </div>
-
-                    <div className="note">
-                      <div className="status">
-                        <p>Please Download the Attachments and read it before signing.</p>
-                      </div>
-                    </div>
-
-                    {/* table */}
-                    <div className="sign-button">
-                      {document.notary === caller.address && !document.notarySigned ? (
-                        <Button
-                          color="green"
-                          icon
-                          labelPosition="left"
-                          onClick={() => notarizeDocument(document.docId, document.hash)}
-                        >
-                          <Icon name="signup" />
-                          Notarize
-                        </Button>
-                      ) : !document.partySigned && !sign ? (
-                        <Button
-                          color="green"
-                          icon
-                          labelPosition="left"
-                          onClick={() => signDocument(document.hash, document.docId)}
-                        >
-                          <Icon name="signup" />
-                          Sign Document
-                        </Button>
-                      ) : (
-                        <Button disabled color="green">
-                          Sign Document
-                        </Button>
-                      )}
-                    </div>
-
-                    {/* ********* */}
-                  </div>
-                </div>
-              </DetailsInfo>
-            ) : (
-              <Loader active size="medium">
-                Loading
-              </Loader>
-            )}
-          </DocumentHeader>
-          <DocumentContainer>
-            {!loading ? (
-              <DetailsInfo>
-                <div class="Details-card">
-                  <div class="Details-card-info">
-                    <div class="title-left">
-                      <img src={LogoAvatar} alt="" srcset="" />
-                      <h3 className="title-message">{document.title}</h3>
-                    </div>
-
-                    <div class="progress-container ">
-                      <span class="heading">Shared Date</span>
-                      <h3 className="created-by">{document.timestamp}</h3>
-                    </div>
-
-                    <div className="actions">
-                      <div className="status">
-                        <div className="docs-icon">
-                          <img src={Attachments} alt="" srcset="" />
+          {signWarning ? (
+            <SignWarning signWarning={signWarning} setSignWarning={setSignWarning} />
+          ) : (
+            <>
+              {" "}
+              <DocumentHeader>
+                {!loading ? (
+                  <DetailsInfo>
+                    <div class=" header-wrapper">
+                      <div class="Details-card-info header-container">
+                        <div class="title-left">
+                          <h3 className=" header-title">{document.title}</h3>
                         </div>
-                        <h6 className="heading">{document.fileName}</h6>
-                      </div>
 
-                      <div
-                        className="status"
-                        className="docs-icon"
-                        onClick={() => downloadFile(document.title, document.key, document.documentLocation)}
-                      >
-                        {!downloading ? (
-                          <>
-                            <div>
-                              <img src={Download} alt="" srcset="" />
-                            </div>
-                            <h6 className="heading">Download</h6>
-                          </>
-                        ) : (
-                          <>
-                            <div>
-                              <Loader active inline="centered" />
-                            </div>
-                            <h6 className="heading" style={{ fontWeight: "bold" }}>
-                              Downloading
-                            </h6>
-                          </>
-                        )}
-                      </div>
+                        <div class="progress-container ">
+                          {/* conditional rendering */}
+                          {document.signStatus ? <Label color="green">Signed</Label> : <Label>Pending</Label>}
+                        </div>
 
-                      {document.notary ? (
-                        document.notarySigned ? (
+                        <div className="note">
                           <div className="status">
-                            <div className="docs-icon">
-                              <img src={Sign} alt="" srcSet="" />
-                            </div>
-                            <h6 className="heading">
-                              <Icon name="circle" color="green" /> Notarized{" "}
-                            </h6>
+                            <p>Please Download the Attachments and read it before signing.</p>
                           </div>
-                        ) : (
-                          <div className="status">
-                            <div className="docs-icon">
-                              <img src={Sign} alt="" srcSet="" />
-                            </div>
-                            <h6 className="heading">
-                              <Icon name="circle" color="red" /> Not Notarized{" "}
-                            </h6>
-                          </div>
-                        )
-                      ) : null}
+                        </div>
+
+                        {/* table */}
+                        <div className="sign-button">
+                          {document.notary === caller.address && !document.notarySigned && !signWarning ? (
+                            <Button
+                              color="green"
+                              icon
+                              labelPosition="left"
+                              onClick={() => notarizeDocument(document.docId, document.hash)}
+                              loading={signLoader}
+                            >
+                              <Icon name="signup" />
+                              Notarize
+                            </Button>
+                          ) : !document.partySigned && !signWarning ? (
+                            <Button
+                              color="green"
+                              loading={signLoader}
+                              icon
+                              labelPosition="left"
+                              onClick={() => signDocument(document.hash, document.docId)}
+                            >
+                              <Icon name="signup" />
+                              Sign Document
+                            </Button>
+                          ) : (
+                            <Button disabled color="green">
+                              Sign Document
+                            </Button>
+                          )}
+                        </div>
+
+                        {/* ********* */}
+                      </div>
                     </div>
+                  </DetailsInfo>
+                ) : (
+                  <Loader active size="medium">
+                    Loading
+                  </Loader>
+                )}
+              </DocumentHeader>
+              <DocumentContainer>
+                {!loading ? (
+                  <DetailsInfo>
+                    <div class="Details-card">
+                      <div class="Details-card-info">
+                        <div class="title-left">
+                          <img src={LogoAvatar} alt="" srcset="" />
+                          <h3 className="title-message">{document.title}</h3>
+                        </div>
 
-                    {/* table */}
+                        <div class="progress-container ">
+                          <span class="heading">Shared Date</span>
+                          <h3 className="created-by">{document.timestamp}</h3>
+                        </div>
 
-                    <Table singleLine striped>
-                      <Table.Header>
-                        <Table.Row>
-                          <Table.HeaderCell className="table-header">Signers</Table.HeaderCell>
-                          <Table.HeaderCell className="table-header"> Status</Table.HeaderCell>
-                          <Table.HeaderCell className="table-header">Signed On</Table.HeaderCell>
-                        </Table.Row>
-                      </Table.Header>
+                        <div className="actions">
+                          <div className="status">
+                            <div className="docs-icon">
+                              <img src={Attachments} alt="" srcset="" />
+                            </div>
+                            <h6 className="heading">{document.fileName}</h6>
+                          </div>
 
-                      <Table.Body>
-                        {document.sharedTo.map(value => {
-                          return (
-                            <Table.Row>
-                              <Link to={`/profile/${encodeURIComponent(value.did)}`}>
-                                <Table.Cell className="table-header">
-                                  <Image avatar src={jenny} />
-                                  {value.name}
-                                </Table.Cell>
-                              </Link>
-                              <Table.Cell>
+                          <div
+                            className="status"
+                            className="docs-icon"
+                            onClick={() => downloadFile(document.title, document.key, document.documentLocation)}
+                          >
+                            {!downloading ? (
+                              <>
                                 <div>
-                                  {value.partySigned ? (
-                                    <>
-                                      <Icon name="circle" color="green" /> Signed
-                                    </>
-                                  ) : (
-                                    <>
-                                      <Icon name="circle" color="red" /> Pending
-                                    </>
-                                  )}
+                                  <img src={Download} alt="" srcset="" />
                                 </div>
-                              </Table.Cell>
+                                <h6 className="heading">Download</h6>
+                              </>
+                            ) : (
+                              <>
+                                <div>
+                                  <Loader active inline="centered" />
+                                </div>
+                                <h6 className="heading" style={{ fontWeight: "bold" }}>
+                                  Downloading
+                                </h6>
+                              </>
+                            )}
+                          </div>
 
-                              <Table.Cell className="table-header">{value.timestamp}</Table.Cell>
+                          {document.notary ? (
+                            document.notarySigned ? (
+                              <div className="status">
+                                <div className="docs-icon">
+                                  <img src={Sign} alt="" srcSet="" />
+                                </div>
+                                <h6 className="heading">
+                                  <Icon name="circle" color="green" /> Notarized{" "}
+                                </h6>
+                              </div>
+                            ) : (
+                              <div className="status">
+                                <div className="docs-icon">
+                                  <img src={Sign} alt="" srcSet="" />
+                                </div>
+                                <h6 className="heading">
+                                  <Icon name="circle" color="red" /> Not Notarized{" "}
+                                </h6>
+                              </div>
+                            )
+                          ) : null}
+                        </div>
+
+                        {/* table */}
+
+                        <Table singleLine striped>
+                          <Table.Header>
+                            <Table.Row>
+                              <Table.HeaderCell className="table-header">Signers</Table.HeaderCell>
+                              <Table.HeaderCell className="table-header"> Status</Table.HeaderCell>
+                              <Table.HeaderCell className="table-header">Signed On</Table.HeaderCell>
                             </Table.Row>
-                          );
-                        })}
-                      </Table.Body>
-                    </Table>
+                          </Table.Header>
 
-                    {/* ********* */}
-                  </div>
-                </div>
-              </DetailsInfo>
-            ) : (
-              <Loader active size="medium">
-                Loading
-              </Loader>
-            )}
-          </DocumentContainer>
+                          <Table.Body>
+                            {document.sharedTo.map(value => {
+                              return (
+                                <Table.Row>
+                                  <Link to={`/profile/${encodeURIComponent(value.did)}`}>
+                                    <Table.Cell className="table-header">
+                                      <Image avatar src={jenny} />
+                                      {value.name}
+                                    </Table.Cell>
+                                  </Link>
+                                  <Table.Cell>
+                                    <div>
+                                      {value.partySigned ? (
+                                        <>
+                                          <Icon name="circle" color="green" /> Signed
+                                        </>
+                                      ) : (
+                                        <>
+                                          <Icon name="circle" color="red" /> Pending
+                                        </>
+                                      )}
+                                    </div>
+                                  </Table.Cell>
+
+                                  <Table.Cell className="table-header">{value.timestamp}</Table.Cell>
+                                </Table.Row>
+                              );
+                            })}
+                          </Table.Body>
+                        </Table>
+
+                        {/* ********* */}
+                      </div>
+                    </div>
+                  </DetailsInfo>
+                ) : (
+                  <Loader active size="medium">
+                    Loading
+                  </Loader>
+                )}
+              </DocumentContainer>
+            </>
+          )}
         </>
       )}
     </>
--- packages/react-app/src/components/warnings/SignWarning.js
+++ packages/react-app/src/components/warnings/SignWarning.js
@@ -2,24 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Header, Icon, Modal } from "semantic-ui-react";
 
-function SignWarning({ sign, setSign }) {
+function SignWarning({ signWarning, setSignWarning }) {
   return (
-    <Modal closeIcon open={true} onClose={() => setSign(false)} onOpen={() => setSign(true)}>
+    <Modal closeIcon open={true} onClose={() => setSignWarning(false)} onOpen={() => setSignWarning(true)}>
       <Header icon="warning sign" content="OOPS" />
       <Modal.Content>
-        <p>There was an error while Signing. Do you want to Sign Again?</p>
+        <p>Unexpected Error occured while Signing/Notarizing the Document. Try Again!!</p>
       </Modal.Content>
       <Modal.Actions>
-        <Link to="/documents">
-          <Button color="red" onClick={() => setSign(false)}>
-            <Icon name="remove" /> No
-          </Button>
-        </Link>
-        <Link to="/documents">
-          <Button color="green" onClick={() => setSign(false)}>
-            <Icon name="checkmark" /> Yes
-          </Button>
-        </Link>
+        <Button
+          color="green"
+          onClick={() => {
+            setSignWarning(false);
+          }}
+        >
+          Ok
+        </Button>
       </Modal.Actions>
     </Modal>
   );
